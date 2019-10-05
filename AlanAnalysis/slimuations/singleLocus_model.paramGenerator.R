ded17aceca7ce7a9b156a917e305f6e39298044d
Alan Bergland <aob2x@virginia.edu>
2019-10-05 10:06:35 -0600

    fine scaled doobies

--- AlanAnalysis/slimuations/singleLocus_model.paramGenerator.R
+++ AlanAnalysis/slimuations/singleLocus_model.paramGenerator.R
@@ -1,5 +1,5 @@
 N = c(10000)  #population size
-CR = seq(from=.01, to=.99, by=.1) #cloning rate
+CR = c(seq(from=.01, to=.99, by=.1), seq(from=.001, to=.099, by=.01)+.9) #cloning rate
 AFR = seq(from=.01, to=.99, by=.05) #A female rate
 BFR = seq(from=.01, to=.99, by=.05) #B female rate
 
