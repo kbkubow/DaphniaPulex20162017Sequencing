d72edefac50f0c99b26b5757b2bb0792c864289b
Alan Bergland <aob2x@virginia.edu>
2019-10-15 09:15:48 -0400

    updated params

--- AlanAnalysis/slimuations/singleLocus_model.paramGenerator.R
+++ AlanAnalysis/slimuations/singleLocus_model.paramGenerator.R
@@ -1,11 +1,13 @@
+library(data.table)
+
 N = c(10000)  #population size
 CR = c(seq(from=.01, to=.99, by=.1), seq(from=.001, to=.099, by=.01)+.9) #cloning rate
-AFR = seq(from=.01, to=.99, by=.05) #A female rate
-BFR = seq(from=.01, to=.99, by=.05) #B female rate
+AMR = seq(from=.01, to=.99, by=.05) #A female rate
+BMR = seq(from=.01, to=.99, by=.05) #B female rate
 
 
-dt <- as.data.table(expand.grid(N, CR, AFR, BFR))
-setnames(dt, names(dt), c("N", "CR", "AFR", "BFR"))
+dt <- as.data.table(expand.grid(N, CR, AMR, BMR))
+setnames(dt, names(dt), c("N", "CR", "AMR", "BMR"))
 dt <- cbind(data.table(id=c(1:dim(dt)[1])), dt)
 
 write.csv(dt, quote=F, row.names=F,
