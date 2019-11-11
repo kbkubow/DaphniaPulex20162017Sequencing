92c3ea9729695b57efa3a415ed2521be63e18aab
Alan Bergland <aob2x@virginia.edu>
2019-11-11 11:32:45 -0500

    Update pooled_MF.sh

--- AlanAnalysis/pooledMF/pooled_MF.sh
+++ AlanAnalysis/pooledMF/pooled_MF.sh
@@ -3,7 +3,7 @@
 #SBATCH -J pooledMF # A single job name for the array
 #SBATCH --ntasks-per-node=1 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 6:00:00 ### 6 hours
+#SBATCH -t 0:15:00 ### 6 hours
 #SBATCH --mem 1G
 #SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOutput/pooledMF.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/daphnia_hwe_sims/slurmOutput/pooledMF.%A_%a.err # Standard error
@@ -13,4 +13,4 @@
 
 module load gcc/7.1.0  openmpi/3.1.4 R/3.6.1
 Rscript /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/pooledMF/pooled_MF.R \
-1000  ${SLURM_ARRAY_TASK_ID}
+1000 ${SLURM_ARRAY_TASK_ID}
