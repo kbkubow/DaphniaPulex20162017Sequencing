f69be201068378b7bba319bdac2cc03ea39197fd
Alan Bergland <aob2x@virginia.edu>
2020-01-30 13:33:40 -0500

    Update doHarp.sh

--- AlanAnalysis/AB_phasing/doHarp.sh
+++ AlanAnalysis/AB_phasing/doHarp.sh
@@ -40,8 +40,8 @@ module load gcc/7.1.0  openmpi/3.1.4 boost/1.60.0
   sample=$( echo $bam | rev  | cut -f1 -d'/' | rev | cut -f1 -d'.' | cut -f3 -d"_" )
 
 # harp parameters
-  window_step=10000
-  window_width=200000
+  window_step=5000
+  window_width=50000
   #window_step=100
   #window_width=100
 
