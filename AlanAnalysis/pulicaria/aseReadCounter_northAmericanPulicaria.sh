96caf87b8963bd9dd58f4f4353c56a7f2dade472
Alan Bergland <aob2x@virginia.edu>
2020-02-07 15:04:28 -0500

    Update aseReadCounter_northAmericanPulicaria.sh

--- AlanAnalysis/pulicaria/aseReadCounter_northAmericanPulicaria.sh
+++ AlanAnalysis/pulicaria/aseReadCounter_northAmericanPulicaria.sh
@@ -28,15 +28,15 @@ module load gatk/4.0.0.0
 #gatk IndexFeatureFile -F /scratch/aob2x/daphnia_hwe_sims/trioPhase/testTrio.consensus.header.phase.noNA.vcf
 #gatk IndexFeatureFile -F /scratch/aob2x/daphnia_hwe_sims/trioPhase/testTrio.consensus.header.phase.allvariant.vcf
 
-#cat /nv/vol186/bergland-lab/doerthe/2019_NovaSeq_Males_BACKUP/03_gvcfs_vcf/Males_2018_filtsnps10bpindels_snps_filter_pass_lowGQmiss.vcf | awk '{
-#    if(substr($1, 0, 1)=="#") {
-#      print $0
-#    } else {
-#      for(i=1; i<=9; i++) printf $i"\t"
-#      printf "0/1\n"
-#    }
-#}' > /scratch/aob2x/daphnia_hwe_sims/trioPhase/testTrio.consensus.header.phase.allvariant2.vcf
-#
+cat /nv/vol186/bergland-lab/doerthe/2019_NovaSeq_Males_BACKUP/03_gvcfs_vcf/Males_2018_filtsnps10bpindels_snps_filter_pass_lowGQmiss.vcf | awk '{
+    if(substr($1, 0, 1)=="#") {
+      print $0
+    } else {
+      for(i=1; i<=9; i++) printf $i"\t"
+       printf "0/1\t0/1\t0/1\t0/1\t0/1\t0/1\t0/1\t0/1\t0/1\n"
+    }
+}' > /scratch/aob2x/daphnia_hwe_sims/trioPhase/testTrio.consensus.header.phase.allvariant2.vcf
+
 gatk IndexFeatureFile -F /scratch/aob2x/daphnia_hwe_sims/trioPhase/testTrio.consensus.header.phase.allvariant2.vcf
 
 
