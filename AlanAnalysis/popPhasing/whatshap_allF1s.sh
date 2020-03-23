ff688c3a61b7f52a7fe8a917a1a2f92ac96b59cc
Alan Bergland <aob2x@virginia.edu>
2020-03-23 08:09:20 -0400

    Update whatshap_allF1s.sh

--- AlanAnalysis/popPhasing/whatshap_allF1s.sh
+++ AlanAnalysis/popPhasing/whatshap_allF1s.sh
@@ -5,11 +5,11 @@
 #SBATCH --ntasks-per-node=1 # one core
 #SBATCH -N 1 # on one node
 #SBATCH --cpus-per-task=1 ### standard has 28 or 40 $SLURM_CPUS_PER_TASK
-#SBATCH -t 3-6:00:00 # Running time of 2 hours
+#SBATCH -t 4-0:00:00 # Running time of 2 hours
 #SBATCH --mem 64G # Memory request of 12 GB
 #SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOut/popPhasing_whatshapp.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/daphnia_hwe_sims/slurmOut/popPhasing_whatshapp.%A_%a.err # Standard error
-#SBATCH -p largemem
+#SBATCH -p parallel
 #SBATCH --account berglandlab
 
 ### run with: sbatch /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/popPhasing/whatshap_allF1s.sh
