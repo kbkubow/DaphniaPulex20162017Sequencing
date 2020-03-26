1cf1df9aa217c699aa7d3524b9f8384b478f86aa
Alan Bergland <aob2x@virginia.edu>
2020-03-26 10:29:44 -0400

    Update whatshap_prepareSplitVCF.sh

--- AlanAnalysis/popPhasing/whatshap_prepareSplitVCF.sh
+++ AlanAnalysis/popPhasing/whatshap_prepareSplitVCF.sh
@@ -13,20 +13,31 @@
 #SBATCH --account berglandlab
 
 ### run with: sbatch /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/popPhasing/whatshap_prepareSplitVCF.sh
-## sacct -u aob2x -j 9995453
+## sacct -u aob2x -j 9995455
 
 #ijob -c1 -p standard -A berglandlab
 module load gcc/7.1.0 openmpi/3.1.4 python/3.6.8 anaconda/5.2.0-py3.6 samtools htslib
 export PATH=$HOME/.local/bin:$PATH
 
 ### first, bgzip and tabix vcf file
-  bgzip \
-  -c -i -@ 10 \
-  /scratch/aob2x/daphnia_hwe_sims/popPhase/MapDec19PulexandObtusaC_filtsnps10bpindels_snps_filter_pass_lowGQmiss_ann.vcf > \
-  /scratch/aob2x/daphnia_hwe_sims/popPhase/MapDec19PulexandObtusaC_filtsnps10bpindels_snps_filter_pass_lowGQmiss_ann.vcf.gz
+  # bgzip \
+  # -c -i -@ 10 \
+  # /scratch/aob2x/daphnia_hwe_sims/popPhase/MapDec19PulexandObtusaC_filtsnps10bpindels_snps_filter_pass_lowGQmiss_ann.vcf > \
+  # /scratch/aob2x/daphnia_hwe_sims/popPhase/MapDec19PulexandObtusaC_filtsnps10bpindels_snps_filter_pass_lowGQmiss_ann.vcf.gz
 
+  tabix -p vcf /scratch/aob2x/daphnia_hwe_sims/popPhase/MapDec19PulexandObtusaC_filtsnps10bpindels_snps_filter_pass_lowGQmiss_ann.vcf.gz
 
+### make sub-vcf files for each of the large chromosomes
 
-### get 12 big chromosomes
-cat /scratch/aob2x/daphnia_hwe_sims/harp_pools/jobId | cut -f2 -d' ' \
-| sort | uniq | grep -v "chr" | sed 's/^/--chromosome /g' | sed 's/$/ \\/g'
+### get 12 big chromosomes, and split VCF file
+  chr=$( cat /scratch/aob2x/daphnia_hwe_sims/harp_pools/jobId | cut -f2 -d' ' | sort | uniq | grep -v "chr" )
+
+  while IFS= read -r line; do
+      echo "... $line ..."
+      tabix \
+      -p vcf \
+      -h \
+      /scratch/aob2x/daphnia_hwe_sims/popPhase/MapDec19PulexandObtusaC_filtsnps10bpindels_snps_filter_pass_lowGQmiss_ann.vcf.gz
+      $line > \
+      /scratch/aob2x/daphnia_hwe_sims/popPhase/MapDec19PulexandObtusaC_filtsnps10bpindels_snps_filter_pass_lowGQmiss_ann.${line}.vcf
+  done <<< "$chr"
