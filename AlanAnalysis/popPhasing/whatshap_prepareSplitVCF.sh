ca6d711ca43976ca5d757224236d92d13edd3996
Alan Bergland <aob2x@virginia.edu>
2020-03-26 10:07:45 -0400

    Update whatshap_prepareSplitVCF.sh

--- AlanAnalysis/popPhasing/whatshap_prepareSplitVCF.sh
+++ AlanAnalysis/popPhasing/whatshap_prepareSplitVCF.sh
@@ -2,11 +2,11 @@
 #
 #
 #SBATCH -J popPhasing_prepareSplitVCF # A single job name for the array
-#SBATCH --ntasks-per-node=10 # one core
+#SBATCH --ntasks=1 # one core
 #SBATCH -N 1 # on one node
-#SBATCH --cpus-per-task=1 ### standard has 28 or 40 $SLURM_CPUS_PER_TASK
-#SBATCH -t 4-0:00:00 # Running time of 2 hours
-#SBATCH --mem 100G # Memory request of 12 GB
+#SBATCH --cpus-per-task=10 ### standard has 28 or 40 $SLURM_CPUS_PER_TASK
+#SBATCH -t 2:00:00 # Running time of 2 hours
+#SBATCH --mem 5G # Memory request of 12 GB
 #SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOut/popPhasing_prepareSplitVCF.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/daphnia_hwe_sims/slurmOut/popPhasing_prepareSplitVCF.%A_%a.err # Standard error
 #SBATCH -p standard
