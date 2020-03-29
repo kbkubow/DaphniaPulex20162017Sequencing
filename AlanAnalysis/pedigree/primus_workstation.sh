480c913cfbe9ad5905bc1fbb583ad0cb973b69d6
Alan Bergland <aob2x@virginia.edu>
2020-03-29 08:48:29 -0400

    lots of things

--- AlanAnalysis/pedigree/primus_workstation.sh
+++ AlanAnalysis/pedigree/primus_workstation.sh
@@ -14,15 +14,12 @@
 
 
 ### run primus: sbatch /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/pedigree/primus.sh
-### sacct -j 10077986
+
 
 module load perl
-
-wd=/scratch/aob2x/daphnia_hwe_sims/pedigree/PRIMUS_v1.9.0/bin
-
 
 ${wd}/run_PRIMUS.pl \
 -i FILE=/scratch/aob2x/daphnia_hwe_sims/pedigree/MapDec19PulexandObtusaandPulicaria_filtsnps10bpindels_snps_filter_pass_lowGQmiss_ann.12chr.LDprune.renameChr.ibd_king.delim \
 IBD0=7 IBD1=8 IBD2=9 PI_HAT=10 \
 -o /scratch/aob2x/daphnia_hwe_sims/pedigree/primusOut/
--v 3
+-v 3 &
