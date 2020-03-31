e3b4a9cd5ce11ffeb3df116caba918722945f33e
Alan Bergland <aob2x@virginia.edu>
2020-03-31 06:45:21 -0400

    Update shapeit4.sh

--- AlanAnalysis/popPhasing/shapeit4.sh
+++ AlanAnalysis/popPhasing/shapeit4.sh
@@ -5,14 +5,14 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20 ### is for multithreading: standard has 28 or 40 $SLURM_CPUS_PER_TASK
 #SBATCH -t 2-00:00:00 # Running time of 4 days
-#SBATCH --mem 20G # Memory request of 20GB
+#SBATCH --mem 100G # Memory request of 20GB
 #SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOut/popPhasing_shapeit4.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/daphnia_hwe_sims/slurmOut/popPhasing_shapeit4.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
 
 ### run with: sbatch --array=1-12 /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/popPhasing/shapeit4.sh
-### sacct -u aob2x -j 10041462
+### sacct -u aob2x -j 10108497
 ### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/popPhasing_shapeit4.10041462_12.err
 #module load gcc/7.1.0 openmpi/3.1.4 python/3.6.8 anaconda/5.2.0-py3.6 samtools htslib bcftools/1.9 gparallel/20170822
 #
@@ -32,6 +32,6 @@ chr=$( cat /scratch/aob2x/daphnia_hwe_sims/harp_pools/jobId | cut -f2 -d' ' | so
   --thread 20 \
   --sequencing \
   --log /scratch/aob2x/daphnia_hwe_sims/popPhase/shapeitOut/${chr}.log \
-  --ibd2-length 5 \
+  --ibd2-length 1 \
   --ibd2-output /scratch/aob2x/daphnia_hwe_sims/popPhase/shapeitOut/${chr}.IBD2blacklist.txt.gz \
   --output /scratch/aob2x/daphnia_hwe_sims/popPhase/shapeitOut/${chr}.whatshapp.shapeit.bcf
