eaacf6ec39e33e8380e5c7d11de0a3d2972d71c3
Alan Bergland <aob2x@virginia.edu>
2020-07-12 16:25:42 -0400

    Update runRabbit.sh

--- AlanAnalysis/rQTL/rabbitRedo/runRabbit.sh
+++ AlanAnalysis/rQTL/rabbitRedo/runRabbit.sh
@@ -11,15 +11,56 @@
 #SBATCH -p standard
 #SBATCH --account berglandlab
 
+### run as
+# sbatch --array=1-12 ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/rQTL/rabbitRedo/runRabbit.sh
+
+
 module load intel/18.0 intelmpi/18.0 R/3.6.3
 module load mathematica/11.1.1
 module load parallel
 
 #SLURM_ARRAY_TASK_ID=2
 
-wd="/scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing"
+wd="/scratch/aob2x/daphnia_hwe_sims"
 datadir="/scratch/aob2x/daphnia_hwe_sims/Rabbit_phase"
 chr=$( grep "^${SLURM_ARRAY_TASK_ID}" ${datadir}/chrs.csv | cut -f2 -d',' )
 echo $chr
 
+### generate RABBIT input data
 Rscript ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/rQTL/rabbitRedo/formatData.R ${chr}
+
+
+### format RABBIT script file
+RABBITpackageLocation="/scratch/aob2x/daphnia_hwe_sims/RABBIT/RABBIT_Packages/"
+RABBITmodel="jointModel"
+RABBITestfun="origViterbiDecoding"
+generations_for_RABBIT=1
+topDirectory="/scratch/aob2x/daphnia_hwe_sims/trioPhase"
+
+# Generate mathematica script for RABBIT
+python - <<EOF > ${datadir}/${chr}.RABBIT.m
+print """SetDirectory["%s"]""" % "${RABBITpackageLocation}"
+print """Needs["MagicReconstruct\`"]"""
+print """Needs["MagicMap\`"]"""
+print """SetDirectory["%s"]""" % "${datadir}"
+print """popScheme = popScheme="ped.ped" """
+print 'model = "%s"' % "${RABBITmodel}"
+print 'estfun = "%s"' % "${RABBITestfun}"
+print 'inputfile = "%s"' % "${chr}.all.in"
+print 'resultFile = "%s.txt"' % "${chr}.out"
+print """magicImpute[inputfile, model, popScheme, isFounderInbred -> False, outputFileID -> resultFile, isPrintTimeElapsed -> True]"""
+print 'imputed = "%s"' % "${chr}.out_ImputedGenotype.csv"
+print """magicReconstruct[imputed, model, popScheme, isFounderInbred -> False, outputFileID -> resultFile, reconstructAlgorithm -> estfun, isPrintTimeElapsed -> True]"""
+print """summaryFile = StringDrop[resultFile, -4] <> ".csv" """
+print """saveAsSummaryMR[resultFile<>"_magicReconstruct.txt", summaryFile]"""
+print 'Exit'
+EOF
+
+
+### Run RABBIT impute & reconstruct
+math -script ${datadir}/${chr}.RABBIT.m
+
+### convert paths
+python /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/rQTL/parseHaplotypes.py \
+${chr}.all.csv \
+/scratch/aob2x/ >> ${chr}.all.haps
