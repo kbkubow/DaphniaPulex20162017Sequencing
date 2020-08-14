78c8e5df227a22c9243c8a65f25167b0414a0107
Alan Bergland <aob2x@virginia.edu>
2020-08-14 13:10:07 -0400

    treemix

--- AlanAnalysis/rQTL/rabbitRedo/plotRabbit.R
+++ AlanAnalysis/rQTL/rabbitRedo/plotRabbit.R
@@ -6,11 +6,20 @@
   library(foreach)
   library(SeqArray)
 
+### make SNP table
+genofile <- seqOpen("/project/berglandlab/Karen/MappingDec2019/WithPulicaria/June2020/MapJune2020_ann.seq.gds", allow.duplicate=TRUE)
+snp.dt <- data.table(chr=seqGetData(genofile, "chromosome"),
+                     pos=seqGetData(genofile, "position"),
+                     id=seqGetData(genofile, "variant.id"),
+                     numAlleles=seqNumAllele(genofile))
+setkey(snp.dt, id)
+
 ### load posterior probabilities
   setwd("/scratch/aob2x/daphnia_hwe_sims/Rabbit_phase_10cm")
+
   loadDat <- function(fn) {
     print(fn)
-    #fn <- fns[1]
+    #fn <- fns[11]
     pp <- fread(fn, skip="magicReconstruct-Summary,genoprob,Conditonal genotype probability", header=T, fill=T)
 
     setnames(pp, names(pp), as.character(pp[1,]))
@@ -39,31 +48,46 @@
 
     ppl <- merge(ppl, codes, by="founder")
     ppl[,chr:=gsub(".all.out.post.csv", "", fn)]
+
+    setnames(ppl, "variable", "id")
+    ppl[,id:=as.numeric(as.character(id))]
+    setkey(ppl, id)
+    m <- merge(ppl, snp.dt)
+
     return(ppl)
   }
   fns <- list.files("/scratch/aob2x/daphnia_hwe_sims/Rabbit_phase_10cm", pattern=".out.post.csv")
   ppl <- foreach(x=fns)%do%loadDat(x)
   ppl <- rbindlist(ppl)
 
-  ppl.ag <- ppl[,list(founder=geno[which.max(value2)]), list(variable, clone, chr)]
+
+  save(ppl, file="/project/berglandlab/ppl.Rdata")
 
 
-### make snp.dt
-  genofile <- seqOpen("/project/berglandlab/Karen/MappingDec2019/WithPulicaria/June2020/MapJune2020_ann.seq.gds", allow.duplicate=TRUE)
-  snp.dt <- data.table(chr=seqGetData(genofile, "chromosome"),
-                       pos=seqGetData(genofile, "position"),
-                       id=seqGetData(genofile, "variant.id"),
-                       numAlleles=seqNumAllele(genofile),
-                       key="chr")
-  ppl.ag <- merge(ppl.ag, snp.dt, by.x="variable", by.y="id")
-  write.csv(ppl.ag, file="/project/berglandlab/alan/AxC_F1.csv", quote=F, row.names=F)
+  load(file="/project/berglandlab/ppl.Rdata")
+
+  setnames(ppl, "variable", "id")
+  ppl[,id:=as.numeric(as.character(id))]
+  ppl.ag <- ppl[,list(founder=geno[which.max(value2)]), list(id, clone, chr)]
+
+  setkey(ppl.ag, id)
+  setkey(snp.dt, id)
+
+  m <- merge(ppl.ag, snp.dt)
+
+  table(m$chr.x==m$chr.y)
+
+
+  write.csv(m, file="/project/berglandlab/alan/AxC_F1.csv", quote=F, row.names=F)
 
 ### plot
+
   plotOut <- ggplot() +
-  geom_tile(data=ppl, aes(x=variable, y=founder, fill=value2)) +
-  geom_line(data=ppl.ag, aes(x=variable, y=founder, group=clone), color="red") +
+  geom_tile(data=ppl, aes(x=id, y=founder, fill=value2)) +
+  geom_line(data=m, aes(x=id, y=founder, group=clone), color="red") +
   facet_grid(chr.x~clone) +
-  scale_fill_viridis() +
   theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
         axis.ticks.x=element_blank())
+
+  ggsave(plotOut, file="~/plotOut.png")
