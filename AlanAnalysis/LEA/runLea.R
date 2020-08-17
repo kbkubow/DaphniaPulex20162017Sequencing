7d75a3ceb8e9e905a76e178a9d9500f69ab87cae
Alan Bergland <aob2x@virginia.edu>
2020-08-17 12:52:35 -0400

    updates to lea

--- AlanAnalysis/LEA/runLea.R
+++ AlanAnalysis/LEA/runLea.R
@@ -1,9 +1,8 @@
 library(LEA)
 dap.snmf <- snmf("/scratch/aob2x/daphnia_hwe_sims/daps4lea.geno",
     K=c(1:10),
-    project = "continue",
-    repetitions = 1, CPU = 1,
-    alpha = 10, tolerance = 0.00001, entropy = FALSE, percentage = 0.05, I=10000, iterations = 200, ploidy = 2, seed = -1)
+    project = "new",
+    CPU = 10, entropy=T)
 
 
 save(dap.snmf, file="/scratch/aob2x/daphnia_hwe_sims/dap.snmf.Rdata")
