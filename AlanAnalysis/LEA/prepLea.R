85e4705fdbf275dface9d5a5b234925d57b1867f
Alan Bergland <aob2x@virginia.edu>
2020-08-17 21:41:17 -0400

    udpates

--- AlanAnalysis/LEA/prepLea.R
+++ AlanAnalysis/LEA/prepLea.R
@@ -37,8 +37,84 @@ setwd("/project/berglandlab/Karen/MappingDec2019/WithPulicaria/June2020/")
             force = TRUE)
 
 ### ancestry test
-snmf("/scratch/aob2x/daphnia_hwe_sims/daps4lea.geno",
-    K=c(1:10),
-    project = "continue",
-    repetitions = 1, CPU = 1,
-    alpha = 10, tolerance = 0.00001, entropy = FALSE, percentage = 0.05, I=10000, iterations = 200, ploidy = 2, seed = -1)
+#snmf("/scratch/aob2x/daphnia_hwe_sims/daps4lea.geno",
+#    K=c(1:10),
+#    project = "continue",
+#    repetitions = 1, CPU = 1,
+#    alpha = 10, tolerance = 0.00001, entropy = FALSE, percentage = 0.05, I=10000, iterations = 200, ploidy = 2, seed = -1)
+
+#obj.snmf = snmf("/scratch/aob2x/daphnia_hwe_sims/daps4lea.geno", K = 3, alpha = 100, project = "new")
+
+
+
+#### post process
+library(LEA)
+library(foreach)
+library(data.table)
+
+load(file="/scratch/aob2x/daphnia_hwe_sims/dap.snmf.Rdata")
+	q.list <- foreach(k=1:10)%do%{
+		Q(dap.snmf, K = k)
+	}
+
+
+setwd("/project/berglandlab/Karen/MappingDec2019/WithPulicaria/June2020/")
+
+### load metadata file
+  samps <- fread("Superclones201617182019withObtusaandPulicaria_kingcorr_20200623_wmedrd.txt")
+  samps[,set:=paste(Species, population, sep="_")]
+
+
+
+
+	samps.vcf <- fread("/scratch/aob2x/daphnia_hwe_sims/daps4lea.vcf", skip="#CHROM", nrows=1)
+	samps.vcf <- names(samps.vcf)[-(1:9)]
+	setkey(samps, clone)
+
+	samps <- samps[J(samps.vcf)]
+	samps[,x:=1:dim(samps)[1]]
+
+save(q.list, samps, file="/scratch/aob2x/Q_samps.Rdata")
+
+#scp aob2x@rivanna.hpc.virginia.edu:/scratch/aob2x/Q_samps.Rdata ~/Q_samps.Rdata
+# R
+library(LEA)
+library(foreach)
+library(data.table)
+library(ggplot2)
+library(pophelper)
+library(cowplot); theme_set(theme_cowplot())
+load("~/Q_samps.Rdata")
+
+processQ <- function(k, samp) {
+	qi<-as.data.table(q.list[[k]])
+	qi <- as.data.table(qi)
+	qi[,gr:=apply(qi, 1, which.max)]
+	qi[,samp:=samp]
+	qil <- melt(qi, measure.vars=paste("V", 1:k, sep=""))
+
+	qil.ag <- foreach(gr.i=unique(qil$gr), .combine="rbind")%do%{
+		qil.sub <- qil[gr==gr.i][variable==paste("V", gr.i, sep="")]
+		qil.sub[,ord:=rank(value, ties.method="first")]
+		qil.sub
+	}
+
+	setkey(qil, samp)
+	setkey(qil.ag, samp)
+
+	qil <- merge(qil, qil.ag)
+	qil[,ordf:=factor(ord, levels=sort(unique(ord)))]
+	setnames(qil, "samp", "clone")
+	qil
+}
+
+
+k <- processQ(k=5, samp=samps$clone)
+k <- merge(k, samps, by="clone")
+
+
+ggplot(k, aes(x=ord, y=value.x, fill=as.factor(variable.x))) +
+geom_bar(position="stack", stat="identity") +
+facet_grid(~gr.y, scale="free_x", space="free") +
+geom_text(data=k[Species=="pulicaria"], aes(x=ord, y=1, label="*"), size=2) +
+geom_text(data=k[Species=="obtusa"], aes(x=ord, y=1, label="$"), size=2)
--- AlanAnalysis/LEA/runLea.sh
+++ AlanAnalysis/LEA/runLea.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 #
 #SBATCH -J split_and_run # A single job name for the array
-#SBATCH --ntasks-per-node=1 # one core
+#SBATCH --ntasks-per-node=10 # one core
 #SBATCH -N 1 # on one node
 #SBATCH -t 2:00:00 ### most jobs should run in 60 minutes or less; the mitochondria takes a lot longer to run through pool-snp
 #SBATCH --mem 5G
-##SBATCH -o /scratch/aob2x/dest/slurmOutput/split_and_run.%A_%a.out # Standard output
-##SBATCH -e /scratch/aob2x/dest/slurmOutput/split_and_run.%A_%a.err # Standard error
+##SBATCH -o /scratch/aob2x/lea.%A_%a.out # Standard output
+##SBATCH -e /scratch/aob2x/lea.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
 
 # sbatch ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/LEA/runLea.sh
-# sacct -j 14320063
+# sacct -j 14350702
 
 module load intel/18.0  intelmpi/18.0 R/3.6.3
 
