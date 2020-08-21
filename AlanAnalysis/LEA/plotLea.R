9aa91b8a184112409019ee0a4e99c4dce60fd33b
Alan Bergland <aob2x@virginia.edu>
2020-08-21 07:34:33 -0600

    final

--- AlanAnalysis/LEA/plotLea.R
+++ AlanAnalysis/LEA/plotLea.R
@@ -15,18 +15,18 @@
   library(cowplot); theme_set(theme_cowplot())
 
 ### load datda
-  load("~/Q_samps.Rdata")
-  load("~/dap.snmf.Rdata")
-
+  #load("~/Q_samps.Rdata")
+  #load("~/dap.snmf.Rdata")
+  load("~/snmf_out.Rdata")
 
 ### which K?
-  plot(dap.snmf)
-
+  ggplot(data=ce.dt, aes(x=k, y=ce, group=run, color=as.factor(run))) + geom_line()
+  ce.dt[k<10][which.min(ce)]
 
 ### extract Q matrix
-  processQ <- function(k, samp, orderby) {
-    #k<-3; samp=samps$clone; orderby="set"
-  	qi<-as.data.table(q.list[[k]])
+  processQ <- function(k, samp, run, orderby) {
+    #k<-8; run=8; samp=samps$clone; orderby="set"
+  	qi<-as.data.table(q.list[[k]][[run]])
   	qi <- as.data.table(qi)
   	qi[,gr:=apply(qi, 1, which.max)]
   	qi[,samp:=samp]
@@ -47,6 +47,9 @@
       qil.ag <- foreach(set.i=unique(qil$set), .combine="rbind")%do%{
         #set.i<-"pulex_D8"
         qil.temp <- qil[set==set.i]
+        qil.temp.ave <- qil.temp[,list(mu=mean(value)), list(variable)]
+
+
         qil.temp.ag <- qil.temp[,list(gr=unique(gr), max=max(value)), list(clone)]
         keycol <-c("gr", "max")
         setorderv(qil.temp.ag, keycol)
@@ -79,8 +82,6 @@
     ggplot(k.i, aes(x=ord, y=value, fill=as.factor(variable))) +
     geom_bar(position="stack", stat="identity", width = 1) +
     facet_grid(~set, scale="free_x", space="free") +
-    geom_text(data=k.i[Species=="pulicaria"], aes(x=ord, y=1, label="*"), size=2) +
-    geom_text(data=k.i[Species=="obtusa"], aes(x=ord, y=1, label="$"), size=2) +
     geom_text(data=k.i[SC=="A"], aes(x=ord, y=1.1, label="A"), size=5) +
     geom_text(data=k.i[SC=="C"], aes(x=ord, y=1.1, label="C"), size=5) +
 
@@ -93,6 +94,57 @@
     guides(fill=FALSE)
 
   }
+
+  plotQ.sp <- function(k.i) {
+    k.i[SC=="A",col:="A"]
+    k.i[SC=="C",col:="C"]
+    setorderv(k.i, "col")
+
+    puli.k <- k.i[Species=="pulicaria"][which.max(value)]$variable
+    obt.k <- k.i[Species=="obtusa"][which.max(value)]$variable
+
+    puli.plot <- ggplot(data=k.i[variable==puli.k], aes(x=set, y=log10(value), color=col)) +
+    geom_jitter(width = 0.25) +
+    #geom_jitter(data=k.i[variable==puli.k][SC=="A"], aes(x=set, y=log10(value)), color="blue", size=1, width = 0.25) +
+    #geom_jitter(data=k.i[variable==puli.k][SC=="C"], aes(x=set, y=log10(value)), color="red", size=1, width = 0.25) +
+    coord_flip() +
+    labs(title=puli.k) +
+    theme(legend.position="none")
+
+    obt.plot <- ggplot(data=k.i[variable==obt.k], aes(x=set, y=log10(value), color=col)) +
+    geom_jitter(width = 0.25) +
+    #geom_jitter(data=k.i[variable==puli.k][SC=="A"], aes(x=set, y=log10(value)), color="blue", size=1, width = 0.25) +
+    #geom_jitter(data=k.i[variable==puli.k][SC=="C"], aes(x=set, y=log10(value)), color="red", size=1, width = 0.25) +
+    coord_flip() +
+    labs(title=obt.k) +
+    theme(legend.position="none")
+
+
+    puli.plot | obt.plot
+
+  }
+
+
+
+### summary plot
+  ce.dt[k==8][which.min(ce)]
+
+  k.i<-8; run.i<-8
+  kr <- processQ(k=k.i, run=run.i, samp=samps$clone, orderby="set")
+  q.plot <- plotQ(kr)
+  out.plot <- plotQ.sp(kr)
+  ce.plot <-   ggplot(data=ce.dt, aes(x=k, y=ce, group=run, color=as.factor(run))) +
+               geom_line() +
+               geom_point(data=ce.dt[k==k.i][run==run.i], aes(x=k, y=ce), size=3, color="black") +
+               theme(legend.position="none")
+
+
+layout <- "
+AAAAAAAA
+##BBBBCC
+"
+  q.plot / ( out.plot | ce.plot) +
+  plot_layout(design = layout)
 
 
 plot.q1 <- plotQ(processQ(k=1, samp=samps$clone, orderby="set"))
--- AlanAnalysis/LEA/runLea.R
+++ AlanAnalysis/LEA/runLea.R
@@ -1,12 +1,13 @@
 library(LEA)
 
 dap.snmf <- snmf("/scratch/aob2x/daphnia_hwe_sims/daps4lea.geno",
-    K=c(1:10),
-    project = "continue",
-    repetitions=10,
+    K=c(1:20),
+    project = "new",
+    repetitions=30,
     CPU = 10,
     entropy=T,
-    I=20000)
+    I=20000,
+    iterations = 100000)
 
 
 save(dap.snmf, file="/scratch/aob2x/daphnia_hwe_sims/dap.snmf.Rdata")
