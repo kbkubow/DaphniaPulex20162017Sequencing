6d9a0dc4489c3f5125f3a8400893cbd164cf4bc0
Alan Bergland <aob2x@virginia.edu>
2020-10-26 07:53:38 -0400

    updates

--- AlanAnalysis/rQTL/lme4qtl.gwas.sh
+++ AlanAnalysis/rQTL/lme4qtl.gwas.sh
@@ -23,3 +23,4 @@ module load gcc/7.1.0 openmpi/3.1.4 R/3.6.3
 wd="/scratch/aob2x/daphnia_hwe_sims"
 
 Rscript ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/rQTL/lme4qtl.gwas.R ${SLURM_ARRAY_TASK_ID} AxC
+Rscript ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/rQTL/lme4qtl.gwas.R ${SLURM_ARRAY_TASK_ID} all
