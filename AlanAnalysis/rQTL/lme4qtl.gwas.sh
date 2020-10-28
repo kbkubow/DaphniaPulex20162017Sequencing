4d3f29c469d0fed4b5197179ee252f55eb97078e
Alan Bergland <aob2x@virginia.edu>
2020-10-27 21:16:13 -0400

    Update lme4qtl.gwas.sh

--- AlanAnalysis/rQTL/lme4qtl.gwas.sh
+++ AlanAnalysis/rQTL/lme4qtl.gwas.sh
@@ -13,9 +13,9 @@
 
 
 ### run as
-# sbatch --array=1-50 ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/rQTL/lme4qtl.gwas.sh
+# sbatch --array=1-100 ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/rQTL/lme4qtl.gwas.sh
 # sacct -j 17884349 #AxC
-# sacct -j 17884561 #CxC
+# sacct -j 17976410 #CxC
 # cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/lme4qtl.17773556_8.err
 
 module load gcc/7.1.0 openmpi/3.1.4 R/3.6.3
@@ -23,5 +23,5 @@ module load gcc/7.1.0 openmpi/3.1.4 R/3.6.3
 #SLURM_ARRAY_TASK_ID=1
 wd="/scratch/aob2x/daphnia_hwe_sims"
 
-#Rscript ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/rQTL/lme4qtl.gwas.R ${SLURM_ARRAY_TASK_ID} AxC
-Rscript ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/rQTL/lme4qtl.gwas.R ${SLURM_ARRAY_TASK_ID} CxC
+Rscript ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/rQTL/lme4qtl.gwas.R ${SLURM_ARRAY_TASK_ID} AxC
+#Rscript ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/rQTL/lme4qtl.gwas.R ${SLURM_ARRAY_TASK_ID} CxC
