e09d3240607cf3626015fc543719fdb5b3493a32
Alan Bergland <aob2x@virginia.edu>
2020-11-28 20:17:01 -0500

    Update mergeVCF.sh

--- AlanAnalysis/popPhasing/mergeVCF.sh
+++ AlanAnalysis/popPhasing/mergeVCF.sh
@@ -3,7 +3,7 @@
 ##SBATCH -J popPhasing_mergeVCF # A single job name for the array
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20 ### is for multithreading: standard has 28 or 40 $SLURM_CPUS_PER_TASK
-#SBATCH -t 0-00:00:15 # Running time of 4 days
+#SBATCH -t 0-02:00:00 # Running time of 4 days
 #SBATCH --mem 100G # Memory request of 20GB
 #SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOut/popPhasing_mergeVCF.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/daphnia_hwe_sims/slurmOut/popPhasing_mergeVCF.%A_%a.err # Standard error
@@ -12,8 +12,8 @@
 
 # ijob -c1 -p standard -A berglandlab
 ### run with: sbatch --array=1-12 /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/popPhasing/mergeVCF.sh
-### sacct -u aob2x -j 19045385
-### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/popPhasing_mergeVCF.19045385_1.err
+### sacct -u aob2x -j 19047983
+### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/popPhasing_mergeVCF.19047983_1.out
 
 ### load modules
   module load gcc/7.1.0 openmpi/3.1.4 python/3.6.8 anaconda/5.2.0-py3.6 samtools htslib bcftools/1.9 gparallel/20170822
