571e071a7d12ada1667b588cd9526a50267d7710
Alan Bergland <aob2x@virginia.edu>
2020-12-02 16:42:05 -0500

    updates

---
+++ AlanAnalysis/popPhasing/analysis.collect.trees.R
@@ -0,0 +1,48 @@
+#module load gcc/7.1.0  openmpi/3.1.4 R/3.6.3; R
+
+
+### libraries
+  library(data.table)
+  library(foreach)
+  library(ape)
+  library(doMC)
+  registerDoMC(20)
+
+### input files: genome-wide distribution
+
+
+  fn <- list.files("/scratch/aob2x/daphnia_hwe_sims/popPhase/trees/", "Rdata", full.names=T)
+  lenfth(fn)
+  cdl.o <- foreach(i=fn)%dopar%{
+    message(i)
+    #i <- fn[1]
+    load(i)
+    #return(cdl)
+    cdl[,window:=tstrsplit(i1, ";")[[2]]]
+
+    cdl[,list(cd_mean=mean(cd), cd_sd=sd(cd)), list(sp.group, pond.group, window)]
+  }
+  cdl.genome <- rbindlist(cdl.o)
+
+### qtl
+  wins <- fread("/scratch/aob2x/daphnia_hwe_sims/popPhase/windows.delim")
+
+  target.fn <- sapply(tail(wins, 14)$V2, function(x) fn[grepl(x, fn)])
+
+  lenfth(target.fn)
+  cdl.qtl <- foreach(i=target.fn)%dopar%{
+    message(i)
+    #i <- fn[1]
+    load(i)
+    #return(cdl)
+    cdl[,window:=tstrsplit(i1, ";")[[2]]]
+
+    #cdl[,list(cd_mean=mean(cd), cd_sd=sd(cd)), list(sp.group, pond.group, window)]
+  }
+  cdl.qtl <- rbindlist(cdl.qtl)
+
+
+### save
+
+
+  save(cdl.genome, cdl.qtl, file="~/cdlo.Rdata")
---
+++ AlanAnalysis/popPhasing/analysis.collectTrees.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+##SBATCH -J maketree # A single job name for the array
+#SBATCH --ntasks=20
+#SBATCH --cpus-per-task=1 ### is for multithreading: standard has 28 or 40 $SLURM_CPUS_PER_TASK
+#SBATCH -t 0-02:00:00 # Running time of 4 days
+#SBATCH --mem 20G # Memory request of 20GB
+#SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOut/maketree.%A_%a.out # Standard output
+#SBATCH -e /scratch/aob2x/daphnia_hwe_sims/slurmOut/maketree.%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+
+### sbatch /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/popPhasing/analysis.collectTrees.sh
+### sacct -u aob2x -j 19144597
+### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/popPhasing_mergeVCF.19110025_10.err
+
+### sbatch --array=234 /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/popPhasing/analysis.makeTrees.sh
+### sacct -u aob2x -j 19143682
+### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/popPhasing_mergeVCF.19110025_10.err
+
+### modules
+  module load samtools parallel
+  module load gcc/7.1.0  openmpi/3.1.4 R/3.6.3
+
+  Rscript --vanilla /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/popPhasing/analysis.collect.trees.R
