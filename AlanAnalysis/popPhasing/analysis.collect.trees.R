adac0ef74f182dc642d6283a377ecba0c4aa0c0a
Alan Bergland <aob2x@virginia.edu>
2020-12-03 11:07:54 -0500

    duh

--- AlanAnalysis/popPhasing/analysis.collect.trees.R
+++ AlanAnalysis/popPhasing/analysis.collect.trees.R
@@ -13,7 +13,7 @@
 
   fn <- list.files("/scratch/aob2x/daphnia_hwe_sims/popPhase/trees/", "Rdata", full.names=T)
   length(fn)
-  cdl.o <- foreach(i=fn[1:5])%dopar%{
+  cdl.o <- foreach(i=fn)%dopar%{
     message(i)
     #i <- fn[1]
     load(i)
