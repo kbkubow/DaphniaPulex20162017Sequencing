626c8bb33233281f486e61aa6a2d6c30828e6d2f
Alan Bergland <aob2x@virginia.edu>
2020-12-03 11:07:54 -0500

    duh

--- AlanAnalysis/popPhasing/analysis.collectTrees.sh
+++ AlanAnalysis/popPhasing/analysis.collectTrees.sh
@@ -11,7 +11,7 @@
 #SBATCH --account berglandlab
 
 ### sbatch /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/popPhasing/analysis.collectTrees.sh
-### sacct -u aob2x -j 19157690
+### sacct -u aob2x -j 19158308
 ### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/maketree.19149417
 
 ### modules
