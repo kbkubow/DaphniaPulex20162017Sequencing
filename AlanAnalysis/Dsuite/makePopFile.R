da5defb73cc358ae331903bc60b24ea61d2bb12f
Alan Bergland <aob2x@virginia.edu>
2020-12-06 09:02:48 -0500

    updates

--- AlanAnalysis/Dsuite/makePopFile.R
+++ AlanAnalysis/Dsuite/makePopFile.R
@@ -7,7 +7,20 @@
   library(SeqArray)
 
 ### open GDS file
-  #genofile <- seqOpen("/project/berglandlab/Karen/MappingDec2019/MapDec19PulexandObtusaandPulicaria_filtsnps10bpindels_snps_filter_pass_lowGQmiss_ann.seq.gds")
+  genofile <- seqOpen("/project/berglandlab/Karen/MappingDec2019/WithPulicaria/June2020/MapJune2020_ann.seq.gds", allow.duplicate=TRUE)
+
+  snpFilter <- fread("/scratch/aob2x/daphnia_hwe_sims/popPhase/daphnid.sites")
+  setnames(snpFilter, c("V1", "V2"), c("chr", "pos"))
+  snp.dt <- data.table(chr=seqGetData(genofile, "chromosome"),
+                       pos=seqGetData(genofile, "position"),
+                       id=seqGetData(genofile, "variant.id"),
+                       numAlleles=seqNumAllele(genofile),
+                       key="chr")
+  setkey(snpFilter, chr, pos)
+  setkey(snp.dt, chr, pos)
+
+  snp.dt <- merge(snpFilter, snp.dt)
+
 
 ### clone metadata file
   sc <- fread("/project/berglandlab/Karen/MappingDec2019/WithPulicaria/June2020/Superclones201617182019withObtusaandPulicaria_kingcorr_20200623_wmedrd.txt")
@@ -15,23 +28,27 @@
   sc <- sc[Nonindependent==0]
 
   sc[,SC.uniq:=SC]
-  sc[SC=="OO", SC.uniq:=paste(SC, SCnum, sep="")]
+  sc[SC=="OO" , SC.uniq:=paste(SC, SCnum, sep="")]
 
 ### hard filtering of SC
-  sc.ag <- sc[LabGenerated==F, list(clone=clone[which.max(medrd)][1]), list(SC.uniq, Species)]
+  sc.ag <- sc[LabGenerated==F & Species=="pulex", list(clone=clone[which.max(medrd)][1]), list(SC.uniq, Species)]
   #sc.ag[,clone:=paste(Species, clone, sep="_")]
 
 
-sc.ag[,pond:=tstrsplit(clone, "_")[[3]]]
-sc.ag[,gr:=pond]
-sc.ag[Species!="pulex", gr:=Species]
-sc.ag[gr=="obtusa", gr:="Outgroup"]
+  sc.ag[,pond:=tstrsplit(clone, "_")[[3]]]
+  sc.ag[,gr:=pond]
+
+  use <- sc.ag[,c("clone", "gr"),with=F]
+  use <- rbind(use, data.table(clone=c("pulicaria", "obtusa"), gr=c("pulicaria", "obtusa")))
+  use[clone=="obtusa", gr:="outgroup"]
 
 ### write to disk
-  write.table(sc.ag[,c("clone", "gr"),with=F], file="/scratch/aob2x/daphnia_hwe_sims/dsuite/SETS.txt", quote=F, row.names=F, col.names=F, sep="\t")
+  write.table(use, file="/scratch/aob2x/daphnia_hwe_sims/dsuite/hybrid_strategy.txt", quote=F, row.names=F, col.names=F, sep="\t")
 
+### write vcf file
+  seqSetFilter(genofile, sample.id=sc.ag$clone, variant.id=snp.dt$id)
 
-
+  seqGDS2VCF(genofile, vcf.fn="/scratch/aob2x/daphnia_hwe_sims/dsuite/daphnids.orig.vcf", info.var=character(), fmt.var=character())
 
 #### write to disk
 #  write.table(sc.ag, file="/scratch/aob2x/daphnia_hwe_sims/popPhase/representativeSC.delim", quote=F, row.names=F, sep=",")
