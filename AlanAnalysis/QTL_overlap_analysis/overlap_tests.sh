35bae7ae8bd6359abb271ea0f2a922b72a55612e
Alan Bergland <aob2x@virginia.edu>
2021-01-13 12:01:21 -0500

    overlap test

--- AlanAnalysis/QTL_overlap_analysis/overlap_tests.sh
+++ AlanAnalysis/QTL_overlap_analysis/overlap_tests.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 #
-#SBATCH -J overlap_test # A single job name for the array
+#SBATCH -J lme4qtl # A single job name for the array
 #SBATCH --ntasks-per-node=1 # one core
 #SBATCH -N 1 # on one node
-#SBATCH --cpus-per-task=1 ### standard has 28 or 40 $SLURM_CPUS_PER_TASK
-#SBATCH -t 0:20:00 # Running time of 1 hours
-#SBATCH --mem 10G # Memory request of 8 GB
-#SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOut/overlap_test.%A_%a.out # Standard output
-#SBATCH -e /scratch/aob2x/daphnia_hwe_sims/slurmOut/overlap_test.%A_%a.err # Standard error
+#SBATCH --cpus-per-task=10 ### standard has 28 or 40 $SLURM_CPUS_PER_TASK
+#SBATCH -t 7:00:00 # Running time of 1 hours
+#SBATCH --mem 12G # Memory request of 8 GB
+#SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOut/lme4qtl.%A_%a.out # Standard output
+#SBATCH -e /scratch/aob2x/daphnia_hwe_sims/slurmOut/lme4qtl.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
 
@@ -23,5 +23,5 @@ module load gcc/7.1.0 openmpi/3.1.4 R/3.6.3
 #SLURM_ARRAY_TASK_ID=1
 wd="/scratch/aob2x/daphnia_hwe_sims"
 
-Rscript ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/QTL_overlap_analysis/overlap_tests.R ${SLURM_ARRAY_TASK_ID}
+Rscript ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/rQTL/lme4qtl.gwas.R ${SLURM_ARRAY_TASK_ID} AxC
 #Rscript ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/rQTL/lme4qtl.gwas.R ${SLURM_ARRAY_TASK_ID} CxC
