8be34fc9c4b59360b220e415099f5d3256b8d146
Alan Bergland <aob2x@virginia.edu>
2021-02-13 17:53:04 -0500

    Update ASEReadCounter.sh

--- AlanAnalysis/RNAseq/ASEReadCounter.sh
+++ AlanAnalysis/RNAseq/ASEReadCounter.sh
@@ -9,8 +9,8 @@
 #SBATCH -o /scratch/aob2x/daphnia_hwe_sims/harp_pools/slurmOut/ASE_readcounter.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/daphnia_hwe_sims/harp_pools/slurmOut/ASE_readcounter.%A_%a.err # Standard error
 
-# submit as: sbatch --array=1-4 /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/pooledMF/aseReadCounter_mf.sh
-# sacct -j 17864732
+# submit as: sbatch --array=1-8 /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/RNAseq/ASEReadCounter.sh
+# sacct -j 20416102
 # cat /scratch/aob2x/daphnia_hwe_sims/harp_pools/slurmOut/ASE_readcounter.17863582_1.out
 
 module load gatk/4.0.0.0
@@ -20,18 +20,17 @@ module load gatk/4.0.0.0
 
 #gatk IndexFeatureFile -F /scratch/aob2x/daphnia_hwe_sims/AC_sites.vcf
 
+#SLURM_ARRAY_TASK_ID=2
+wd=/scratch/aob2x/daphnia_hwe_sims/
+
+samp=$( sed "${SLURM_ARRAY_TASK_ID}q;d" ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/RNAseq/samples )
+echo $samp
+
 
 ###
 
 gatk ASEReadCounter \
---I /scratch/aob2x/daphnia_hwe_sims/rnaseq/bam/d8_179_1.trim.bam \
---I /scratch/aob2x/daphnia_hwe_sims/rnaseq/bam/d8_179_2.trim.bam \
---I /scratch/aob2x/daphnia_hwe_sims/rnaseq/bam/d8_222_1.trim.bam \
---I /scratch/aob2x/daphnia_hwe_sims/rnaseq/bam/d8_222_2.trim.bam \
---I /scratch/aob2x/daphnia_hwe_sims/rnaseq/bam/d8_349_1.trim.bam \
---I /scratch/aob2x/daphnia_hwe_sims/rnaseq/bam/d8_349_2.trim.bam \
---I /scratch/aob2x/daphnia_hwe_sims/rnaseq/bam/d8_515_1.trim.bam \
---I /scratch/aob2x/daphnia_hwe_sims/rnaseq/bam/d8_515_2.trim.bam \
+--I /scratch/aob2x/daphnia_hwe_sims/rnaseq/bam/${samp}.trim.bam \
 --variant /scratch/aob2x/daphnia_hwe_sims/AC_sites.vcf \
---output /scratch/aob2x/daphnia_hwe_sims/rnaseq/rnaseq_asereadcounter.delim \
+--output /scratch/aob2x/daphnia_hwe_sims/rnaseq/ase/${samp}_rnaseq_asereadcounter.delim \
 --reference /project/berglandlab/Karen/MappingDec2019/totalHiCwithallbestgapclosed.fa
