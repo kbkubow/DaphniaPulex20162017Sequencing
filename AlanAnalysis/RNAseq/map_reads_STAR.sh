3d2af19d60d71c1cc8bfb2d8b9fba53fc03efffd
Alan Bergland <aob2x@virginia.edu>
2021-02-14 20:55:47 -0500

    Update map_reads_STAR.sh

--- AlanAnalysis/RNAseq/map_reads_STAR.sh
+++ AlanAnalysis/RNAseq/map_reads_STAR.sh
@@ -10,8 +10,8 @@
 #SBATCH -p standard
 #SBATCH --account berglandlab
 
-### sbatch --array=1-8 /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/RNAseq/map_reads.sh
-### sacct -u aob2x -j 20407463
+### sbatch --array=1-8 /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/RNAseq/map_reads_STAR.sh
+### sacct -u aob2x -j 20445383
 ### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.20387599_1.err
 
 module load star/2.7.2b
@@ -43,4 +43,5 @@ STAR \
 --outSAMtype BAM SortedByCoordinate \
 --outFileNamePrefix /scratch/aob2x/daphnia_hwe_sims/rnaseq/bam/${samp}_star \
 --genomeLoad LoadAndKeep \
---limitBAMsortRAM 20000000000
+--limitBAMsortRAM 20000000000 \
+--runThreadN 20
