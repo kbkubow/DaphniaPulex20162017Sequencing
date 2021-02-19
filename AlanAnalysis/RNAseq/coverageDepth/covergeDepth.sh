b004b21061ed1cb3e2290b4764f0b1c9e752c7f8
Alan Bergland <aob2x@virginia.edu>
2021-02-19 07:52:08 -0500

    Update covergeDepth.sh

--- AlanAnalysis/RNAseq/coverageDepth/covergeDepth.sh
+++ AlanAnalysis/RNAseq/coverageDepth/covergeDepth.sh
@@ -3,8 +3,8 @@
 ##SBATCH -J maketree # A single job name for the array
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1 ### is for multithreading: standard has 28 or 40 $SLURM_CPUS_PER_TASK
-#SBATCH -t 0-00:30:00 # Running time of 4 days
-#SBATCH --mem 10G # Memory request of 20GB
+#SBATCH -t 0-00:10:00 # Running time of 4 days
+#SBATCH --mem 2G # Memory request of 20GB
 #SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.%A_%a.err # Standard error
 #SBATCH -p standard
