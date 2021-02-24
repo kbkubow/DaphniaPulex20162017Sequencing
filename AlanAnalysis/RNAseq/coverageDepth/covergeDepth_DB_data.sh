81a74447dd13709211b5eac6932b5b7fced87a5c
Alan Bergland <aob2x@virginia.edu>
2021-02-24 16:58:00 -0500

    db data

--- AlanAnalysis/RNAseq/coverageDepth/covergeDepth_DB_data.sh
+++ AlanAnalysis/RNAseq/coverageDepth/covergeDepth_DB_data.sh
@@ -3,14 +3,14 @@
 ##SBATCH -J maketree # A single job name for the array
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1 ### is for multithreading: standard has 28 or 40 $SLURM_CPUS_PER_TASK
-#SBATCH -t 0-00:10:00 # Running time of 4 days
+#SBATCH -t 0-02:10:00 # Running time of 4 days
 #SBATCH --mem 2G # Memory request of 20GB
 #SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
 
-### sbatch --array=1-8 /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/RNAseq/coverageDepth/covergeDepth.sh
+### sbatch --array=1 /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/RNAseq/coverageDepth/covergeDepth_DB_data.sh
 ### sacct -u aob2x -j 20798058
 ### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.20798058_1.err
 
@@ -19,12 +19,10 @@ module load samtools gcc/9.2.0  openmpi/3.1.6 python/3.7.7 bedtools/2.29.2
 #SLURM_ARRAY_TASK_ID=2
 wd=/scratch/aob2x/daphnia_hwe_sims/
 
-samp=$( sed "${SLURM_ARRAY_TASK_ID}q;d" ${wd}/DaphniaPulex20162017Sequencing/AlanAnalysis/RNAseq/samples )
-echo $samp
 
-if [ ! -f /scratch/aob2x/daphnia_hwe_sims/rnaseq/bam/${samp}_star_testAligned.sortedByCoord.out.bam.bai ]; then
-  samtools index /scratch/aob2x/daphnia_hwe_sims/rnaseq/bam/${samp}_star_testAligned.sortedByCoord.out.bam
-fi
+cp /project/berglandlab/Karen/RNAseqBams/D86A_merged_sorted.BAM /scratch/aob2x/D86A_merged_sorted.BAM
+samtools index /scratch/aob2x/D86A_merged_sorted.BAM
+
 
 gene=Daphnia00786
 
@@ -52,11 +50,12 @@ stop_win=$( expr $stop + 25000 )
 #
 
 samtools view -b \
-/scratch/aob2x/daphnia_hwe_sims/rnaseq/bam/${samp}_star_testAligned.sortedByCoord.out.bam \
+/scratch/aob2x/D86A_merged_sorted.BAM \
 ${chr}:${start_win}-${stop_win} > \
-/project/berglandlab/alan/bam_slices/${samp}.small.test.bam
+/project/berglandlab/alan/bam_slices/D86A_merged_sorted.small.BAM
 
-samtools index /project/berglandlab/alan/bam_slices/${samp}.small.test.bam
+samtools index /project/berglandlab/alan/bam_slices/D86A_merged_sorted.small.BAM
+
 
 
 
