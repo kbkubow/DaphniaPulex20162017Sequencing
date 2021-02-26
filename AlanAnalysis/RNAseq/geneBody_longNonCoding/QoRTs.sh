2fd09c8f236eebae0786c6320110172d47e0c854
Alan Bergland <aob2x@virginia.edu>
2021-02-26 08:11:17 -0500

    Update QoRTs.sh

--- AlanAnalysis/RNAseq/geneBody_longNonCoding/QoRTs.sh
+++ AlanAnalysis/RNAseq/geneBody_longNonCoding/QoRTs.sh
@@ -3,16 +3,16 @@
 #SBATCH -J maketree # A single job name for the array
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1 ### is for multithreading: standard has 28 or 40 $SLURM_CPUS_PER_TASK
-#SBATCH -t 0-01:00:00 # Running time of 4 days
-#SBATCH --mem 10G # Memory request of 20GB
+#SBATCH -t 0-04:00:00 # Running time of 4 days
+#SBATCH --mem 11G # Memory request of 20GB
 #SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
 
-### sbatch --array=1-8 /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/RNAseq/geneBody_longNonCoding/QoRTs.sh
-### sacct -u aob2x -j 20798058
-### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.20798058_1.err
+### sbatch --array=1,3,4,5,7,8 /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/RNAseq/geneBody_longNonCoding/QoRTs.sh
+### sacct -u aob2x -j 20855796
+### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.20855796_1.err
 
 ###### SLURM_ARRAY_TASK_ID=6
 wd=/scratch/aob2x/daphnia_hwe_sims/
@@ -23,7 +23,7 @@ echo $samp
 
 #wget http://hartleys.github.io/QoRTs/QoRTs.jar
 
-java -jar ~/QoRTs.jar \
+java -jar -Xmx10G ~/QoRTs.jar \
 QC \
 /scratch/aob2x/daphnia_hwe_sims/rnaseq/bam/${samp}_star_testAligned.sortedByCoord.out.bam \
 /project/berglandlab/daphnia_ref/Daphnia.aed.0.6.gtf \
