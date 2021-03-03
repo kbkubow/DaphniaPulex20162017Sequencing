9abddca8becc344d99ebe48a90fc4a199902ef64
Alan Bergland <aob2x@virginia.edu>
2021-03-03 13:16:00 -0500

    Update junctionSeq.sh

--- AlanAnalysis/RNAseq/junctionSeq/junctionSeq.sh
+++ AlanAnalysis/RNAseq/junctionSeq/junctionSeq.sh
@@ -4,15 +4,15 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20 ### is for multithreading: standard has 28 or 40 $SLURM_CPUS_PER_TASK
 #SBATCH -t 0-02:00:00 # Running time of 4 days
-#SBATCH --mem 20G # Memory request of 20GB
+#SBATCH --mem 80G # Memory request of 20GB
 #SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
 
-### sbatch /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/RNAseq/QoRTs/QoRTs.sh
-### sacct -u aob2x -j 20920920
-### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.20920920_8.out
+### sbatch /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/RNAseq/junctionSeq/junctionSeq.sh
+### sacct -u aob2x -j 20931137
+### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.20931137
 
 ###### SLURM_ARRAY_TASK_ID=1
 
