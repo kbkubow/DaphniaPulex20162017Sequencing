a038b0db56022ecc142a807569dc04fd3493cb24
Alan Bergland <aob2x@virginia.edu>
2021-03-03 20:55:21 -0500

    Update QoRTs.sh

--- AlanAnalysis/RNAseq/QoRTs/QoRTs.sh
+++ AlanAnalysis/RNAseq/QoRTs/QoRTs.sh
@@ -4,15 +4,15 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1 ### is for multithreading: standard has 28 or 40 $SLURM_CPUS_PER_TASK
 #SBATCH -t 0-08:00:00 # Running time of 4 days
-#SBATCH --mem 20G # Memory request of 20GB
+#SBATCH --mem 100G # Memory request of 20GB
 #SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.%A_%a.err # Standard error
-#SBATCH -p standard
+#SBATCH -p largemem
 #SBATCH --account berglandlab
 
 ### sbatch --array=8 /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/RNAseq/QoRTs/QoRTs.sh
 ### sacct -u aob2x -j 20920920
-### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.20920920_8.out
+### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.20920920_8.err
 
 ###### SLURM_ARRAY_TASK_ID=1
 wd=/scratch/aob2x/daphnia_hwe_sims/
