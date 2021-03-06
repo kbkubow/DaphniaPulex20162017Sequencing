31da212d9a46ff329c477e4657dd03ade0809715
Alan Bergland <aob2x@virginia.edu>
2021-03-05 22:23:00 -0500

    Update map_reads_STAR.sh

--- AlanAnalysis/RNAseq/refGenome_RNAseq/map_reads_STAR.sh
+++ AlanAnalysis/RNAseq/refGenome_RNAseq/map_reads_STAR.sh
@@ -3,14 +3,14 @@
 ##SBATCH -J maketree # A single job name for the array
 #SBATCH --ntasks=20
 #SBATCH --cpus-per-task=1 ### is for multithreading: standard has 28 or 40 $SLURM_CPUS_PER_TASK
-#SBATCH -t 0-04:00:00 # Running time of 4 days
+#SBATCH -t 0-02:00:00 # Running time of 4 days
 #SBATCH --mem 40G # Memory request of 20GB
 #SBATCH -o /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.%A_%a.err # Standard error
 #SBATCH -p standard
 #SBATCH --account berglandlab
 
-### sbatch --array=1-8 /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/RNAseq/refGenome_RNAseq/map_reads_STAR.sh
+### sbatch --array=1-198 /scratch/aob2x/daphnia_hwe_sims/DaphniaPulex20162017Sequencing/AlanAnalysis/RNAseq/refGenome_RNAseq/map_reads_STAR.sh
 ### sacct -u aob2x -j 20592418
 ### cat /scratch/aob2x/daphnia_hwe_sims/slurmOut/map_reads.20592384_6.err
 
